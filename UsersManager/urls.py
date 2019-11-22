3fcf6b2729c745d75e2762c4c9ca9b452fec1780
machinegk <inickbliznyk@gmail.com>
2019-11-22 02:03:36 +0200

    i definitely added something useful, but i dont remember what

--- UsersManager/urls.py
+++ UsersManager/urls.py
@@ -5,7 +5,7 @@ from .views import register, settings
 
 urlpatterns = [
     path('register/', register, name='register-page'),
-    path('login/', LoginView.as_view(template_name='UsersManager/login.html'), name='login-page'),
+    path('login/', LoginView.as_view(template_name='UsersManager/login.html', redirect_authenticated_user=True), name='login-page'),
     path('logout/', LogoutView.as_view(template_name='UsersManager/logout.html'), name='logout-page'),
     path('settings/', settings, name='settings-page'),
 ]
--- UsersManager/views.py
+++ UsersManager/views.py
@@ -2,10 +2,19 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import RegistrationForm
 from django.http import HttpResponse
+from django.contrib.auth.decorators import user_passes_test, login_required
 
+
+def user_is_not_logged_in(user):
+    return not user.is_authenticated
+
+
+@login_required()
 def settings(request):
     return HttpResponse("Settings page")
 
+
+@user_passes_test(user_is_not_logged_in, redirect_field_name='home-page')
 def register(request):
     if request.method == 'POST':
         form = RegistrationForm(request.POST)
@@ -15,4 +24,4 @@ def register(request):
             return redirect('login-page')
     else:
         form = RegistrationForm()
-    return render (request, 'UsersManager/register.html', {'form' : form})
+    return render(request, 'UsersManager/register.html', {'form': form})
